c038c1ede65863bd00a776f999fc5400989da40d
rchakode <rodrigue.chakode@gmail.com>
2019-08-20 10:07:07 +0200

    improved debug script

--- run-debug.sh
+++ run-debug.sh
@@ -9,22 +9,23 @@ fi
 
 CLUSTER_NAME=$1
 KOA_K8S_API_ENDPOINT=$(kubectl config view -o jsonpath="{.clusters[?(@.name==\"$CLUSTER_NAME\")].cluster.server}")
-if [ "$KOA_K8S_API_ENDPOINT" != "" ]; then
-    KOA_K8S_AUTH_TOKEN=$(kubectl get secrets -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='default')].data.token}"|base64 -d)
-else
+if [ "$KOA_K8S_API_ENDPOINT" == "" ]; then
     echo "cannot find KOA_K8S_API_ENDPOINT"
+    exit 1
 fi
 
-if [ "$KOA_K8S_AUTH_TOKEN" != "" ]; then
-    export KOA_K8S_API_ENDPOINT
-    export KOA_K8S_AUTH_TOKEN
-    export KOA_ENABLE_DEBUG=true
-    export KOA_K8S_API_VERIFY_SSL=false
-    export KOA_BILLING_HOURLY_RATE=9.92
-    export KOA_BILLING_CURRENCY_SYMBOL='$'
-    export KOA_COST_MODEL='CHARGE_BACK'
-    fuser -k 5483/tcp || true
-    ./entrypoint.sh
-else
+KOA_K8S_AUTH_TOKEN=$(kubectl get secrets -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='kube-opex-analytics')].data.token}"|base64 -d)
+if [ "$KOA_K8S_AUTH_TOKEN" == "" ]; then
     echo "cannot find KOA_K8S_AUTH_TOKEN"
+    exit 1
 fi
+
+export KOA_K8S_API_ENDPOINT
+export KOA_K8S_AUTH_TOKEN=
+export KOA_ENABLE_DEBUG=true
+export KOA_K8S_API_VERIFY_SSL=false
+export KOA_BILLING_HOURLY_RATE=9.92
+export KOA_BILLING_CURRENCY_SYMBOL='$'
+export KOA_COST_MODEL='CHARGE_BACK'
+fuser -k 5483/tcp || true
+./entrypoint.sh
