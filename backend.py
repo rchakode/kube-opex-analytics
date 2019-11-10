a41caf6d045f0a2729530d5420059263f3ff2475
rchakode <rodrigue.chakode@gmail.com>
2019-11-10 06:27:32 +0100

    set target version to 0.4.3

--- backend.py
+++ backend.py
@@ -43,7 +43,7 @@ def create_directory_if_not_exists(path):
 
 
 class Config:
-    version = '0.4.2'
+    version = '0.4.3'
     db_round_decimals = 6
     db_non_allocatable = 'non-allocatable'
     db_billing_hourly_rate = '.billing-hourly-rate'
