121791eabbdf8e5880495e53157b093af74164af
Rodrigue Chakode <rchakode@users.noreply.github.com>
2022-10-08 06:30:44 +0200

    Make lint happy

--- backend.py
+++ backend.py
@@ -104,13 +104,13 @@ class Config:
         cost_model_label, cost_model_unit = self.process_cost_model_config()
         with open(str('%s/backend.json' % self.frontend_data_location), 'w') as fd:
             fd.write('{"cost_model":"%s", "currency":"%s"}' % (cost_model_label, cost_model_unit))
-            
+  
         # check listener port
         try:
             self.listener_port = int(os.getenv('KOA_LISTENER_PORT'))
         except:
             self.listener_port = 5483
- 
+
         # handle cacert file if applicable
         if self.k8s_verify_ssl and self.k8s_ssl_cacert and os.path.exists(self.k8s_ssl_cacert):
             self.koa_verify_ssl_option = self.k8s_ssl_cacert
